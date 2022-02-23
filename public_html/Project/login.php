bbe97935f9b927b5bac3a8090e4823b9d114d673
MattToegel <matthew.toegel@njit.edu>
2022-02-23 11:58:16 -0500

    adding login stuff

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -62,8 +62,11 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
                         echo "Welcome $email";
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
                     } else {
                         echo "Invalid password";
+                        
                     }
                 } else {
                     echo "Email not found";
